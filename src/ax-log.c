de24521ab13e6bce2bbf0a72b48cb86a8974a8e7
anaximeno <anaximenobrito@gmail.com>
2023-07-06 22:33:22 -0100

    chore: update log message

--- src/ax-log.c
+++ src/ax-log.c
@@ -25,5 +25,5 @@ static inline const char *level_name(enum ax_log_level_t lvl)
 extern void ax_log(enum ax_log_level_t level, char *message)
 {
     if (level != NONE)
-        fprintf(stderr, "%s%s", level_name(level), message);
+        fprintf(stderr, "%s: %s\n", level_name(level), message);
 }

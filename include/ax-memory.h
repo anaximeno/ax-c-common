d8ebb95d8b060c75800ce69804828bd9e2f9c06c
anaximeno <anaximenobrito@gmail.com>
2023-07-07 03:21:36 -0100

    feat: add axcallocate and axfree macros

--- include/ax-memory.h
+++ include/ax-memory.h
@@ -7,13 +7,15 @@
 /** Allocates memory for the TYPE. */
 #define axlocate(type) (type *)malloc(sizeof(type))
 #define axreallocate(value, type, size) (type *)realloc(value, size)
+#define axcallocate(length, type) (type *)calloc(length, sizeof(type))
+#define axfree(value) ({ free(value); value = NULL; })
 
 /** Returns a copy of the VALUE of its given TYPE*/
 #define axmemdup(value, type) ({            \
     type *_tmp_ = axlocate(type);           \
     if (_tmp_ != NULL)                      \
         memcpy(_tmp_, value, sizeof(type)); \
-    _tmp_;                                   \
+    _tmp_;                                  \
 })
 
 #endif
